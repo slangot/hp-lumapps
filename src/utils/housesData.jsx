7c5b3065f0676d0d1fa0081a2aa630334bdb7e1f
Shawn <shawn17@live.fr>
2022-04-20 23:00:57 +0200

    ✨ houses data file creation

---
+++ src/utils/housesData.jsx
@@ -0,0 +1,20 @@
+const housesData = [
+  {
+    name: "Gryffindor",
+    link: "https://www.bebegavroche.com/media/catalog/product/cache/1/thumbnail/1200x/040ec09b1e35df139433887a97daa66f/w/a/wa043-blason-gryffondor_3.jpg",
+  },
+  {
+    name: "Hufflepuff",
+    link: "https://img.fruugo.com/product/0/50/46790500_max.jpg",
+  },
+  {
+    name: "Ravenclaw",
+    link: "https://img.fruugo.com/product/2/50/46790502_max.jpg",
+  },
+  {
+    name: "Slytherin",
+    link: "https://miro.medium.com/max/500/0*m0pYj25tg4A2mAfU.png",
+  },
+];
+
+export default housesData;
